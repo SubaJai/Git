ef40c435a57a99c11ccf446f440557adeeabce9d
subajai <subathra458@gmail.com>
2021-08-23 13:06:35 +0530

    Folder structure

---
+++ src/orf/iftest/Task1.java
@@ -0,0 +1,32 @@
+package orf.iftest;
+
+import java.util.Iterator;
+
+public class Task1 {
+	public static void main(String[] args) {
+		
+		
+		for (int i = 1; i < 11; i++) {
+			if (i==4 || i==6) {
+			continue;
+			}
+			System.out.println(i);
+		}
+//		int a=100,coolegeId=1000;
+//		
+//		if(a==100&&coolegeId==1000)
+//		{
+//			System.out.println("print age");
+//	
+//		}
+//		else
+//		{
+//			System.out.println("printt college");
+//		}
+	
+	
+	
+	}
+	
+
+}
---
+++ src/org/emp/Employee.java
@@ -0,0 +1,33 @@
+package org.emp;
+
+public class Employee {
+	public static void main(String[] args) {
+		byte age=27;
+		System.out.println("Employee age is"+age);
+		
+		short atmpin=4576;
+		System.out.println("Employee atm pin is"+atmpin);
+		
+		int pincode=46892892;
+		System.out.println("Employee pincode is"+pincode);
+		
+		long mobNum=9876543210l;
+		System.out.println("Employee mob num is"+mobNum);
+		
+		float weight=57.78f;
+		System.out.println("Employee weight is"+weight);
+		
+		double salary=3637833.333;
+		System.out.println("Employee salary is"+salary);
+		
+		char gender='M';
+		System.out.println("Employee gender is"+gender);
+		
+		String name="Arjun";
+		System.out.println("Employee name is"+name);
+		
+		boolean maritalstatus=true;
+		System.out.println("Employee marital status is"+maritalstatus);
+	}
+
+}
